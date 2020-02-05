80dac3b0cf2b1b05ee090e011e47c0c7a83f1fa8
QinSouth <2467491114@qq.com>
2020-02-06 05:44:43 +0800

    02-06项目第12次提交

---
+++ SoftwareFactoryProject_/src/main/java/com/factory/SoftwareFactoryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareFactoryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareFactoryProjectApplication.class, args);
+	}
+
+}
---
+++ SoftwareFactoryProject_/src/test/java/com/factory/SoftwareFactoryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareFactoryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
