c2bd12dc9a903b9ae5d4f5dc8812c646312d2ab7
liujiahui1991652424 <1991652424@qq.com>
2020-03-04 20:58:04 +0800

    2020/3/4/ljh

--- SoftwareFactoryProject_/src/main/java/com/factory/biz/purchasingorderBiz.java
+++ SoftwareFactoryProject_/src/main/java/com/factory/biz/purchasingorderBiz.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.factory.entity.PurchasingOrder;
 import com.factory.entity.PurchasingOrderDetails;
 import com.factory.mapper.PurchasingOrderDetailsMapper;
@@ -62,5 +61,21 @@ public int insertpurchasenote(PurchasingOrder pu) {
 
 public int insertpurchasenot(PurchasingOrder pu) {
 	return dao.insertpurchasenot(pu);
+}
+
+
+public int updatepoDocumentNumber(PurchasingOrder bi){
+	
+	return dao.updatepoDocumentNumber(bi);
+}
+
+public PurchasingOrder selectpoaudition(PurchasingOrder bi){
+	return dao.selectpoaudition(bi);
+}
+
+
+public int updatepoDocumentNumber2(PurchasingOrder bi){
+	
+	return dao.updatepoDocumentNumber2(bi);
 }
 }
